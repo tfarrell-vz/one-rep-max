ae7c6b9e0e80dcaf3105890abf10f8d18455c50a
tfarrell-vz <farrell@abiresearch.com>
2015-05-10 20:06:39 -0400

    Removed form to prevent submission/reload. Trigger the calculation upon click.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@ var one_rep_max = function(weight, reps) {
 	return weight * (1 + reps/30);
 };
 
+var get_entered_values = function() {
+	var values = [];
+	var weight = $('#weight').val();
+	var reps = $('#reps').val();
+	values.push(weight);
+	values.push(reps);
+
+	console.log(values);
+	
+	return values;
+};
+
+$('#calc').on('click', function() {
+	var data = get_entered_values();
+	var result = one_rep_max(data[0], data[1]);
+	$('#result').val(result);
+});
+
+ 
