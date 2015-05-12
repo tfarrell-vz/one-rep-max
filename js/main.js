e02c8d2a4411e193c7f872d971d694d7b6328236
tfarrell-vz <farrell@abiresearch.com>
2015-05-11 22:38:26 -0400

    Provided comments for the js functions; planned the reset function.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+// Calculate one rep max (ORM) using Epley formula
 var one_rep_max = function(weight, reps) {
 	return weight * (1 + reps/30);
 };
 
+// Obtain the entered values for weight and reps. 
 var get_entered_values = function() {
 	var values = {};
 	var weight = $('#weight').val();
@@ -14,10 +16,12 @@ var get_entered_values = function() {
 	return values;
 };
 
+// Calculate ORM upon click, and push the result to the appropriate field. 
 $('#calc').on('click', function() {
 	var data = get_entered_values();
 	var result = one_rep_max(data['weight'], data['reps']);
 	$('#result').val(result);
 });
 
+// TODO: Reset button function. 
  
